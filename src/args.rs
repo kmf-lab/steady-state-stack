07f3266a48cd102f9d5e50241b776e3a25366e52
nathantippy <nathan@tippy.name>
2023-12-06 22:27:06 -0600

    pull out args
    clean up the toml

---
+++ src/args.rs
@@ -0,0 +1,20 @@
+use structopt_derive::StructOpt;
+
+#[derive(StructOpt, Debug, PartialEq)]
+pub struct Opt {
+    #[structopt(
+    short = "n",
+    long = "name",
+    default_value = "unknown"
+    )]
+    peers_string: String,
+
+    #[structopt(short = "a", long = "age", default_value = "0", validator = |p|
+    match p.parse::<u8>() {
+    Ok(i) if i <= 120 => Ok(()),
+    _ => Err(String::from("Age must be 120 or less.")),
+    }
+    )]
+    my_idx: usize
+
+}
