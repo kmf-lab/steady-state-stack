459dd5c27b34799fc809c3c9f5287b713257a9a6
nathantippy <nathan@tippy.name>
2024-01-17 23:21:44 -0600

    Based on Fast/FIX finance protocol add compressed data storage functions

---
+++ src/steady/serialize/byte_buffer_packer.rs
@@ -0,0 +1,170 @@
+
+use std::ops::{Add, Sub};
+use bytes::{Bytes, BytesMut};
+use num_traits::Zero;
+use crate::steady::serialize::fast_protocol_packed::{read_long_signed, read_long_unsigned, write_long_signed, write_long_unsigned};
+
+/// writes the deltas so we can write or send less data across the system.
+/// note vecs must be the same size or larger never smaller as we go
+pub(crate) struct PackedVecWriter<T> {
+    pub(crate) previous: Vec<T>,
+}
+
+impl <T> PackedVecWriter<T>
+    where T: Sub<Output = T> + Into<i128> + Copy + Zero + PartialEq {
+
+    fn consume_to_u64(bits:&Vec<u8>) -> Vec<u64> {
+        let mut u64_values = Vec::with_capacity(1+(bits.len() >> 6));
+        let mut p:usize = 0;
+        while p<bits.len() {
+            let mut value: u64 = 0;
+            for i in 0..64 {
+                if p < bits.len() {
+                    value |= (bits[p] as u64) << i;
+                    p += 1;
+                } else {
+                    break;
+                }
+            }
+            u64_values.push(value);
+        }
+        u64_values
+    }
+    pub(crate) fn add_vec(&mut self, mut target: &mut BytesMut, source: &Vec<T>) {
+        assert_eq!(true, source.len() >= self.previous.len());
+        // which numbers changed? we use a 1 for them
+        let zero = T::zero();
+        let previous_iter = self.previous.iter().chain(std::iter::repeat(&zero));
+        let mut bits:Vec<u8> = source.iter()
+            .zip(previous_iter)
+            .map(|(s, p)| (*s != *p) as u8)
+            .collect();
+        let mut chunks:Vec<u64> = Self::consume_to_u64(&bits);
+        //remove any zeros off the end we do not need them
+        while chunks.len()>0 && 0==chunks[chunks.len()-1] {
+            chunks.truncate(chunks.len()-1);
+        }
+        //write length of chunks (yes unsigned would be better but not much)
+        write_long_unsigned(chunks.len() as u64, &mut target);
+        //write the bit mask for which do have changes
+        chunks.iter().for_each(|c| write_long_unsigned(*c, &mut target));
+        //write each of the deltas
+        self.previous.iter()
+            .zip(source.iter())
+            .for_each(|(p,s)| {
+                let dif:i128 = (*s - *p).into();
+                if 0!=dif {
+                    write_long_signed(dif as i64, &mut target);
+                }
+            });
+
+        self.previous.clear();
+        self.previous.extend_from_slice(source);
+    }
+}
+
+
+pub(crate) struct PackedVecReader<T> {
+    previous: Vec<T>,
+}
+
+impl <T> PackedVecReader<T>
+    where T: From<i64> + Sub<Output = T> + Add<Output = T> + Copy {
+
+    fn restore_vec(&mut self, buffer: &mut Bytes) -> Option<Vec<T>> {
+        // Read the length of chunks
+        let chunks_len = read_long_unsigned(buffer)?;
+
+        // Read the bitmask chunks and reconstruct the bitmask
+        let mut bitmask: Vec<u8> = Vec::new();
+        for _ in 0..chunks_len {
+            let chunk = read_long_unsigned(buffer)?;
+            for i in 0..64 {
+                bitmask.push(((chunk >> i) & 1) as u8);
+            }
+        }
+
+        // Apply differences based on the bitmask to reconstruct the vector
+        let mut result: Vec<T> = Vec::with_capacity(self.previous.len());
+        let mut bitmask_iter = bitmask.into_iter();
+        for &prev in &self.previous {
+            if let Some(bit) = bitmask_iter.next() {
+                result.push( if bit.eq(&0u8) {
+                    prev
+                } else {
+                    let val:i64 = read_long_signed(buffer)?;
+                    prev + T::from(val)
+
+                });
+            } else { //if we have no more bits we have no more changes..
+                result.push( prev );
+            }
+        }
+
+        self.previous.clear();
+        self.previous.extend_from_slice(&result);
+        Some(result)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bytes::{BytesMut, Buf};
+
+
+
+    #[test]
+    fn test_round_trip_single_change() {
+        let mut writer:PackedVecWriter<i128> = PackedVecWriter { previous: vec![1,2,3,4] };
+        let mut reader:PackedVecReader<i128> = PackedVecReader { previous: vec![1,2,3,4] };
+
+        let mut buffer = BytesMut::new();
+        let new_vec:Vec<i128> = vec![1, 2, 3, 5]; // One element changed
+        writer.add_vec(&mut buffer, &new_vec);
+
+        println!("buffer len {:?}",buffer.len());
+
+
+        let mut buffer = buffer.freeze();
+        let restored_vec = reader.restore_vec(&mut buffer);
+
+        assert_eq!(restored_vec.unwrap(), new_vec);
+    }
+
+    #[test]
+    fn test_round_trip_multiple_changes() {
+        let mut writer:PackedVecWriter<i64> = PackedVecWriter { previous: vec![10, 20, 30, 40] };
+        let mut reader:PackedVecReader<i64> = PackedVecReader { previous: vec![10, 20, 30, 40] };
+
+        let mut buffer = BytesMut::new();
+        let new_vec = vec![11, 21, 31, 41]; // All elements changed
+        writer.add_vec(&mut buffer, &new_vec);
+
+        println!("buffer len {:?}",buffer.len());
+
+        let mut buffer = buffer.freeze();
+        let restored_vec = reader.restore_vec(&mut buffer);
+
+        assert_eq!(restored_vec.unwrap(), new_vec);
+    }
+
+    #[test]
+    fn test_round_trip_no_change() {
+        let mut writer:PackedVecWriter<i128> = PackedVecWriter { previous: vec![5, 5, 5, 5] };
+        let mut reader:PackedVecReader<i128> = PackedVecReader { previous: vec![5, 5, 5, 5] };
+
+        let mut buffer = BytesMut::new();
+        let new_vec = vec![5, 5, 5, 5]; // No change
+        writer.add_vec(&mut buffer, &new_vec);
+
+        println!("buffer len {:?}",buffer.len());
+
+        let mut buffer = buffer.freeze().clone();
+        let restored_vec = reader.restore_vec(&mut buffer);
+
+        assert_eq!(restored_vec.unwrap(), new_vec);
+    }
+
+    // Add more tests to cover different scenarios
+}
---
+++ src/steady/serialize/fast_protocol_packed.rs
@@ -0,0 +1,227 @@
+
+//////////////////////////////////////////////////////////
+//FIX FAST (FIX Adapted for Streaming) Decoder and Encoder
+//////////////////////////////////////////////////////////////////////////
+//Write signed long using variable length encoding as defined in FAST spec
+//  NOTE: do not modify this but do duplicate this to build an i128 version.
+//////////////////////////////////////////////////////////////////////////
+
+use bytes::{Buf, BufMut, Bytes};
+use bytes::BytesMut;
+
+pub fn read_long_signed(mut byte_buffer: &mut Bytes) -> Option<i64> {
+    let initial_remaining:usize = byte_buffer.remaining();
+    if initial_remaining>0 {
+        let v = byte_buffer.get_i8();
+        let accumulator = ((!(((v >> 6) & 1) - 1)) as i64) & (0xFFFFFFFFFFFFFF80u64 as i64);
+        if v < 0 {
+            Some(accumulator | (v as i64 & 0x7F))
+        } else {
+            read_long_signed_tail((accumulator | v as i64) << 7, &mut byte_buffer, initial_remaining)
+        }
+    } else {
+        None
+    }
+}
+fn read_long_signed_tail(a: i64, mut byte_buffer: &mut Bytes,initial_remaining:usize) -> Option<i64> {
+    let remaining:usize = byte_buffer.remaining();
+    if remaining>0 {
+        let v = byte_buffer.get_i8();
+        if v < 0 {
+            Some(a | (v as i64 & 0x7F))
+        } else {
+            if initial_remaining-remaining > 10 {
+                None //we found bad data so we are not going to read
+            } else {
+                read_long_signed_tail((a | v as i64) << 7, &mut byte_buffer, initial_remaining)
+            }
+        }
+    } else {
+        None
+    }
+}
+
+pub fn read_long_unsigned(byte_buffer: &mut Bytes) -> Option<u64> {
+    let mut value: u64 = 0;
+    let mut byte_count = 0;
+
+    while byte_buffer.has_remaining() {
+        let byte = byte_buffer.get_u8();
+
+        value = (value<<7) | ((byte & 0x7F) as u64);
+
+        // Check if the stop bit is set
+        if byte & 0x80 != 0 {
+            // If high bit is set, stop reading further
+            return Some(value);
+        }
+
+        // Prevent reading more than expected for a u64 value
+        byte_count += 1;
+        if byte_count > 10 {
+            return None; // Too many bytes, likely incorrect data
+        }
+    }
+
+    // If the buffer ends before the sequence is complete, return None
+    None
+}
+
+#[inline]
+fn write_long_signed_pos(value: u64, byte_buffer: &mut BytesMut) {
+    if value >= 0x0000000000000040 {
+        if value >= 0x0000000000002000 {
+            if value >= 0x0000000000100000 {
+                if value >= 0x0000000008000000 {
+                    if value >= 0x0000000400000000 {
+                        if value >= 0x0000020000000000 {
+                            if value >= 0x0001000000000000 {
+                                if value >= 0x0080000000000000 {
+                                    if value >= 0x4000000000000000 {
+                                        byte_buffer.put_u8(((value >> 63) & 0x7F) as u8);
+                                    }
+                                    byte_buffer.put_u8(((value >> 56) & 0x7F) as u8);
+                                }
+                                byte_buffer.put_u8(((value >> 49) & 0x7F) as u8);
+                            }
+                            byte_buffer.put_u8(((value >> 42) & 0x7F) as u8);
+                        }
+                        byte_buffer.put_u8(((value >> 35) & 0x7F) as u8);
+                    }
+                    byte_buffer.put_u8(((value >> 28) & 0x7F) as u8);
+                }
+                byte_buffer.put_u8(((value >> 21) & 0x7F) as u8);
+            }
+            byte_buffer.put_u8(((value >> 14) & 0x7F) as u8);
+        }
+        byte_buffer.put_u8(((value >> 7) & 0x7F) as u8);
+    }
+    // Always write the last byte
+    byte_buffer.put_u8(((value & 0x7F) | 0x80) as u8);
+}
+
+#[inline]
+fn write_long_signed_neg(value: i64, byte_buffer: &mut BytesMut) {
+    let absv = (-value) as u64;
+    if absv > 0x0000000000000040 {
+        if absv > 0x0000000000002000 {
+            if absv > 0x0000000000100000 {
+                if absv > 0x0000000008000000 {
+                    if absv > 0x0000000400000000 {
+                        if absv > 0x0000020000000000 {
+                            if absv > 0x0001000000000000 {
+                                if absv > 0x0080000000000000 {
+                                    if absv > 0x4000000000000000 {
+                                        byte_buffer.put_u8(((value >> 63) & 0x7F ) as u8);
+                                    }
+                                    byte_buffer.put_u8(((value >> 56) & 0x7F ) as u8);
+                                }
+                                byte_buffer.put_u8(((value >> 49) & 0x7F) as u8);
+                            }
+                            byte_buffer.put_u8(((value >> 42) & 0x7F ) as u8);
+                        }
+                        byte_buffer.put_u8(((value >> 35) & 0x7F ) as u8);
+                    }
+                    byte_buffer.put_u8(((value >> 28) & 0x7F) as u8);
+                }
+                byte_buffer.put_u8(((value >> 21) & 0x7F ) as u8);
+            }
+            byte_buffer.put_u8(((value >> 14) & 0x7F ) as u8);
+        }
+        byte_buffer.put_u8(((value >> 7) & 0x7F ) as u8);
+    }
+    byte_buffer.put_u8((( value & 0x7F) | 0x80) as u8);
+}
+
+pub fn write_long_unsigned(value: u64, byte_buffer: &mut BytesMut) {
+        write_long_signed_pos(value, byte_buffer);
+}
+
+pub fn write_long_signed(value: i64, byte_buffer: &mut BytesMut) {
+    if value >= 0 {
+        write_long_signed_pos(value as u64, byte_buffer);
+    } else {
+        if value != i64::MIN {
+            write_long_signed_neg(value, byte_buffer);
+        } else {
+            byte_buffer.extend_from_slice(&[0x7F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80]);
+        }
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////
+//Read signed long using variable length encoding as defined in FAST spec
+//////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bytes::{Bytes, BytesMut};
+
+    fn encode_decode_unsigned_test(value: u64) {
+        let mut buffer = BytesMut::with_capacity(16);
+        write_long_unsigned(value, &mut buffer);
+        let encoded_bytes = buffer.freeze();
+        //println!("value: {:?} bytes: {:?}",value,&encoded_bytes);
+        let mut read_buffer = Bytes::copy_from_slice(&encoded_bytes);
+        let decoded = read_long_unsigned(&mut read_buffer);
+
+        assert_eq!(value, decoded.unwrap_or( if 0==value {1} else {0}), "Failed at value: {:?} vs {:?}", value,decoded);
+    }
+
+    #[test]
+    fn test_unsigned_boundaries() {
+        encode_decode_unsigned_test(0);
+        encode_decode_unsigned_test(1);
+        encode_decode_unsigned_test(0x3F); // Boundary for 1-byte encoding
+        encode_decode_unsigned_test(0x40); // Boundary for 2-byte encoding
+        encode_decode_unsigned_test(0x2000); // 3-byte encoding
+        encode_decode_unsigned_test(0x100000); // 4-byte
+        encode_decode_unsigned_test(0x8000000); // 5-byte
+        encode_decode_unsigned_test(u64::MAX-2);
+        encode_decode_unsigned_test(u64::MAX-1);
+        encode_decode_unsigned_test(u64::MAX);
+    }
+
+    fn encode_decode_signed_test(value: i64) {
+        let mut buffer = BytesMut::with_capacity(16);
+        write_long_signed(value, &mut buffer);
+        let encoded_bytes = buffer.freeze();
+        //println!("value: {:?} bytes: {:?}",value,&encoded_bytes);
+        let mut read_buffer = Bytes::copy_from_slice(&encoded_bytes);
+        let decoded = read_long_signed(&mut read_buffer);
+
+        assert_eq!(value, decoded.unwrap_or( if 0==value {1} else {0}), "Failed at value: {:?} vs {:?}", value,decoded);
+    }
+
+
+
+    #[test]
+    fn test_signed_boundaries() {
+        // Test common boundary values
+        encode_decode_signed_test(0);
+        encode_decode_signed_test(1);
+        encode_decode_signed_test(i64::MAX);
+        encode_decode_signed_test(i64::MAX-1);
+        encode_decode_signed_test(0x3F); // Boundary for 1-byte encoding
+        encode_decode_signed_test(0x40); // Boundary for 2-byte encoding
+        encode_decode_signed_test(0x2000); // 3-byte encoding
+        encode_decode_signed_test(0x100000); // 4-byte
+        encode_decode_signed_test(0x8000000); // 5-byte
+        encode_decode_signed_test(i64::MIN/2);
+        encode_decode_signed_test( -9223372036854775808);
+        encode_decode_signed_test( -4223372036854775808);
+        encode_decode_signed_test( -5223372036854775808);
+        encode_decode_signed_test(i64::MIN+1);
+        encode_decode_signed_test(i64::MIN);
+        encode_decode_signed_test(-1); // ->  &[0xFF, 0x00];
+        encode_decode_signed_test(-0x40); // Negative boundary for 1-byte encoding
+        encode_decode_signed_test(-0x41); // Negative boundary for 2-byte encoding
+        encode_decode_signed_test(-0x2001); // 3-byte encoding
+        encode_decode_signed_test(-0x100001); // 4-byte
+        encode_decode_signed_test(-0x8000001); // 5-byte
+
+    }
+
+}
+
